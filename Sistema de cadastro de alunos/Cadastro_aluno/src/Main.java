81d1ae553b1bacdede30800c07052ae073847139
RafaelCili <rafaelgcili2019@gmail.com>
2025-08-12 15:33:06 -0300

    Programa melhorado, mas com um erro de dar dois enters no removeStudents

--- Sistema de cadastro de alunos/Cadastro_aluno/src/Main.java
+++ Sistema de cadastro de alunos/Cadastro_aluno/src/Main.java
@@ -1,4 +1,6 @@
 import entities.Student;
+import entities.StudentManager;
+
 import java.util.*;
 
 
@@ -11,133 +13,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         List<Student> students = new ArrayList<>();
+        StudentManager manager = new StudentManager();
 
-        studentsRegister(students, sc);
-        Register(students);
-        searchStudents(students, sc);
-        modifyStudents(students, sc);
-        removeStudent(students, sc);
-        Register(students);
+        manager.studentsRegister(students, sc);
+        manager.Register(students);
+        manager.searchStudents(students, sc);
+        manager.modifyStudents(students, sc);
+        manager.removeStudent(students, sc);
+        manager.Register(students);
+
         sc.close();
 
     }
 
-    public static void studentsRegister(List<Student> students, Scanner sc) {
-        System.out.println("Enter how many students will be registered? ");
-        int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            System.out.printf("Enter the %d° student ID: ", i + 1);
-            int ID = sc.nextInt();
-
-            System.out.printf("Enter the %d° student name: ", i + 1);
-            sc.nextLine();
-            String name = sc.nextLine();
-
-            System.out.printf("Enter the %d° student final note: ", i + 1);
-            double finalNote = sc.nextDouble();
-
-            students.add(new Student(name, ID, finalNote));
-
-        }
-
-    }
-
-    public static void Register(List<Student> students) {
-        int i = 0;
-        for (Student s : students) {
-            i++;
-            System.out.printf("The %d° student\n", i);
-            System.out.println(s.toString());
-
-        }
-    }
-
-    public static void searchStudents(List<Student> students, Scanner sc) {
-        Student student;
-        System.out.println("You want to search a student by their ID (y/n)? ");
-        char choice = sc.next().toLowerCase().charAt(0);
-
-        if (choice == 'y') {
-
-            while (true) {
-                System.out.println("Enter the student ID: ");
-                int ID = sc.nextInt();
-
-                Optional<Student> result = students.stream().filter(x -> x.getID() == ID).findFirst();
-
-                if (result.isPresent()) {
-                    student = result.get();
-                    System.out.println("Name: " + student.getName());
-                    System.out.println("Final note: " + student.getFinalNote());
-                    break;
-                } else {
-                    System.out.println("ID not found. Try again.");
-                }
-            }
-
-        }
-
-
-    }
-
-    public static void modifyStudents(List<Student> students, Scanner sc) {
-        Student student;
-        System.out.println("You wish modify the final note of an student?(y/n)");
-        char choice = sc.next().toLowerCase().charAt(0);
-
-        if (choice == 'y') {
-
-            while (true) {
-                System.out.println("Enter the student ID: ");
-                int ID = sc.nextInt();
-
-                Optional<Student> result = students.stream().filter(x -> x.getID() == ID).findFirst();
-
-                if (result.isPresent()) {
-                    student = result.get();
-                    System.out.println("The student is: " + student.getName());
-                    System.out.println("The final note is " + student.getFinalNote());
-
-                    System.out.println("Enter the new final note: ");
-                    student.setFinalNote(sc.nextDouble());
-
-                    System.out.println("The new final note is " + student.getFinalNote());
-                    break;
-                } else {
-                    System.out.println("ID not found. Try again.");
-                }
-
-            }
-
-        }
-
-
-    }
-
-    public static void removeStudent(List<Student> students, Scanner sc) {
-        System.out.println("You want to remove a student from the list?(y/n)");
-        char choice = sc.next().toLowerCase().charAt(0);
-
-        if(choice == 'y') {
-
-            while (true) {
-                System.out.println("Enter the student ID: ");
-                int ID = sc.nextInt();
-
-                Optional<Student> toRemove = students.stream().filter(x -> x.getID() == ID).findFirst();
-
-                if(toRemove.isPresent()) {
-                    System.out.println("The student who was removed is: " + toRemove.get().getName());
-                    students.remove(toRemove.get());
-                    break;
-                } else {
-                    System.out.println("ID not found. Try again.");
-                }
-            }
-        }
-
-
-
-    }
 }
---
+++ Sistema de cadastro de alunos/Cadastro_aluno/src/entities/StudentManager.java
@@ -0,0 +1,133 @@
+package entities;
+import entities.Student;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class StudentManager {
+
+    public StudentManager() {
+        List<Student> students = new ArrayList<>();
+    }
+
+
+    public void studentsRegister(List<Student> students, Scanner sc) {
+        System.out.println("Enter how many students will be registered? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Enter the %d° student ID: ", i + 1);
+            int ID = sc.nextInt();
+
+            System.out.printf("Enter the %d° student name: ", i + 1);
+            sc.nextLine();
+            String name = sc.nextLine();
+
+            System.out.printf("Enter the %d° student final note: ", i + 1);
+            double finalNote = sc.nextDouble();
+
+            students.add(new Student(name, ID, finalNote));
+
+        }
+
+    }
+
+    public void Register(List<Student> students) {
+        int i = 0;
+        for (Student s : students) {
+            i++;
+            System.out.printf("The %d° student\n", i);
+            System.out.println(s.toString());
+
+        }
+    }
+
+    public void searchStudents(List<Student> students, Scanner sc) {
+        Student student;
+        System.out.println("You want to search a student by their ID (y/n)? ");
+        char choice = sc.next().toLowerCase().charAt(0);
+
+        if (choice == 'y') {
+
+            while (true) {
+                System.out.println("Enter the student ID: ");
+                int ID = sc.nextInt();
+
+                Optional<Student> result = students.stream().filter(x -> x.getID() == ID).findFirst();
+
+                if (result.isPresent()) {
+                    student = result.get();
+                    System.out.println("Name: " + student.getName());
+                    System.out.println("Final note: " + student.getFinalNote());
+                    break;
+                } else {
+                    System.out.println("ID not found. Try again.");
+                }
+            }
+
+        }
+
+
+    }
+
+    public void modifyStudents(List<Student> students, Scanner sc) {
+        Student student;
+        System.out.println("You wish modify the final note of an student?(y/n)");
+        char choice = sc.next().toLowerCase().charAt(0);
+
+        if (choice == 'y') {
+
+            while (true) {
+                System.out.println("Enter the student ID: ");
+                int ID = sc.nextInt();
+
+                Optional<Student> result = students.stream().filter(x -> x.getID() == ID).findFirst();
+
+                if (result.isPresent()) {
+                    student = result.get();
+                    System.out.println("The student is: " + student.getName());
+                    System.out.println("The final note is " + student.getFinalNote());
+
+                    System.out.println("Enter the new final note: ");
+                    student.setFinalNote(sc.nextDouble());
+
+                    System.out.println("The new final note is " + student.getFinalNote());
+                    break;
+                } else {
+                    System.out.println("ID not found. Try again.");
+                }
+
+            }
+
+        }
+
+
+    }
+
+    public void removeStudent(List<Student> students, Scanner sc) {
+        System.out.println("You want to remove a student from the list?(y/n)");
+        char choice = sc.next().toLowerCase().charAt(0);
+
+        if(choice == 'y') {
+
+            while (true) {
+                System.out.println("Enter the student ID: ");
+                int ID = sc.nextInt();
+
+                Optional<Student> toRemove = students.stream().filter(x -> x.getID() == ID).findFirst();
+
+                if(toRemove.isPresent()) {
+                    System.out.println("The student who was removed is: " + toRemove.get().getName());
+                    students.remove(toRemove.get());
+                    break;
+                } else {
+                    System.out.println("ID not found. Try again.");
+                }
+            }
+        }
+
+
+
+    }
+}
